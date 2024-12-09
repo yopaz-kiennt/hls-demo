11ea05192cd5ccd1990e7cdbce45f609c5ed0fa6
ThangPham <thangpd@yopaz.vn>
2024-12-09 08:50:41 +0700

    add rabbitmq

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\ProcessRabbitMQMessage;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $message = [
+            'id' => 123,
+            'email' => 'user@example.com',
+            'name' => 'John Doe',
+        ];
+
+        ProcessRabbitMQMessage::dispatch($message);
+
         return Inertia::render('Admin/Dashboard');
     }
 }
