f06f2684737f4229ff7b662c0787746d6d878e7d
ThangPham <thangpd@yopaz.vn>
2025-01-13 10:24:53 +0700

    fix css

--- resources/js/Components/ui/form/FormMessage.vue
+++ resources/js/Components/ui/form/FormMessage.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { usePage } from '@inertiajs/vue3';
 import { ErrorMessage } from 'vee-validate';
 import { toValue } from 'vue';
 import { useFormField } from './useFormField';
 
 const { name, formMessageId } = useFormField();
+
+const { lang } = usePage().props;
 </script>
 
 <template>
-    <ErrorMessage :id="formMessageId" as="p" :name="toValue(name)" class="text-sm font-medium text-destructive" />
+    <ErrorMessage
+        :id="formMessageId"
+        as="p"
+        :name="toValue(name)"
+        class="font-medium text-destructive"
+        :class="lang === 'en' ? 'text-[13px]' : 'text-[12px]'"
+    />
 </template>
